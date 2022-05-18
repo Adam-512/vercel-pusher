8ea1252d4c43a501bb4cea8cd4e38e6ae0a056a5
adam <ch512006634@gmail.com>
2022-05-18 15:12:18 +0800

    first

---
+++ api/notify.ts
@@ -0,0 +1,22 @@
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import Pusher from 'pusher'
+export default async function getImg(req: VercelRequest, res: VercelResponse) {
+    if (req.method.toLocaleLowerCase() != 'post') {
+        res.status(405).end()
+    }
+    else {
+        let body = req.body
+        console.log('notify content: ', JSON.stringify(body))
+        const pusher = new Pusher({
+            appId: process.env.PUSHER_ID,
+            key: process.env.PUSHER_KEY,
+            secret: process.env.PUSHER_SCRET,
+            cluster: "ap1",
+            useTLS: true
+        });
+        pusher.trigger(body.orderNo, "transfer", {
+            message: body
+        });
+        res.status(200).end()
+    }
+}
